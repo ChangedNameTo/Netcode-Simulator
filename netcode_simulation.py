63742fb682a7bd37f649ec858f62d642cbd18c4f
Will Mitchell <will@neadwerx.com>
2017-09-05 15:17:20 -0400

    master #comment Simulation fully working, needs some tweaks

--- netcode_simulation.py
+++ netcode_simulation.py
@@ -54,6 +54,10 @@ from random import randint
              type=int,
              default=10,
              help="Defines the max kills until 'game' end. Default 10" )
+@click.option("--debug",
+             type=bool,
+             default=False,
+             help="Prints debug information in the top left. Default False" )
 
 # =====================================================================================================
 # ========================================== Function Calls ===========================================
@@ -64,7 +68,7 @@ from random import randint
 # Main function, fires off all other setup functions, starts the simulation
 def main(**kwargs):
     # Grabs the command line args
-    global c1latency, c2latency, c1variance, c2variance, c1packet, c2packet, tickrate, maxkills
+    global c1latency, c2latency, c1variance, c2variance, c1packet, c2packet, tickrate, maxkills, debug
     c1latency = kwargs["c1latency"]
     c2latency = kwargs["c2latency"]
     c1variance = kwargs["c1variance"]
@@ -73,6 +77,7 @@ def main(**kwargs):
     c2packet = kwargs["c2packet"]
     tickrate = 60 / kwargs["tickrate"]
     maxkills = kwargs["maxkills"]
+    debug = kwargs["debug"]
 
     wrapper(start_program)
 
@@ -80,6 +85,7 @@ def start_program(stdscr):
     # Allow things to appear at all
     window_init(stdscr)
 
+    # Initial draw
     draw_screen(stdscr)
 
     # Starts the game
@@ -177,8 +183,8 @@ def player_init(stdscr):
     p1c1location = (winsplitdist / 3)
     p2c1location = (winsplitdist / 3)
     # Server locations
-    p1slocation = (winsplitdist / 2) + winsplitdist
-    p2slocation = (winsplitdist / 2) + winsplitdist
+    p1slocation = (winsplitdist / 3) + winsplitdist
+    p2slocation = (winsplitdist / 3) + winsplitdist
     # Client2 locations
     p1c2location = (winsplitdist / 3) + (winsplitdist * 2)
     p2c2location = (winsplitdist / 3) + (winsplitdist * 2)
@@ -206,7 +212,7 @@ def player_move(stdscr):
         num = randint(1,3)
         movelen = randint(-1, 1)
         combo = num * movelen
-        if(p1c1location + combo > 0 and p1c1location + combo < winwidth):
+        if(p1c1location + combo > 0 and p1c1location + combo < winsplitdist - 5):
             p1c1location = p1c1location + combo
             valid = True
 
@@ -217,7 +223,7 @@ def player_move(stdscr):
         num = randint(1,3)
         movelen = randint(-1, 1)
         combo = num * movelen
-        if(p2c2location + combo > 0 and p2c2location + combo < winwidth):
+        if((p2c2location + combo > (winsplitdist * 2)) and (p2c2location + combo < winwidth - 5)):
             p2c2location = p2c2location + combo
             valid = True
 
@@ -235,7 +241,7 @@ def c1_update_ping():
 def c2_update_ping():
     global c2variance
     global c2ping
-    global nextc1packet
+    global nextc2packet
     global looptick
 
     num = randint(0, c2variance)
@@ -253,16 +259,17 @@ def c2_send_packet(stdscr):
 
 # Checks if the center of player is in front of anywhere on the p1 "hitbox", draws a line if it is
 def p1_shoot_logic():
+    global p2c1location, p1c1location
     # If valid returns true and calls the shot draw
-    if((p1c1location + 3) <= (p2c1location + 5) and (p1c1location + 3) >= (p2c1location)):
+    if((p1c1location + 3) <= (p2c1location + 5)) and ((p1c1location + 3) >= (p2c1location)):
         return True
     else:
         return False
 
 def p2_shoot_logic():
+    global p2c2location, p1c2location
     # If valid returns true and calls the shot draw
-    return True
-    if((p2c2location + 3) <= (p1c2location + 5) and (p2c2location + 3) >= (p1c2location)):
+    if((p2c2location + 3) <= (p1c2location + 5)) and ((p2c2location + 3) >= (p1c2location)):
         return True
     else:
         return False
@@ -295,37 +302,59 @@ def server_process(stdscr):
     global c1kills, c1deaths
     global c2kills, c2deaths
     global c1killmiss, c2killmiss
+    global p1slocation, p2slocation
+    global p2c1location, p1c2location
+    global winsplitdist
+    global nextservertick, tickrate, servertick
+    global c1packet, c2packet
+
+    # Server tick is gonna go up by one
+    servertick = servertick + 1
 
     # Packet loss is a thing in this simulation, so rolls dice to see if someone's packet just disappears
     # |  |(
     # || |_
     num = randint(0, 100)
-    if(num < c1packetloss):
+    if(num < c1packet):
+        c1packetloss = c1packetloss + 1
         c1packetstack = None
     else:
         # Updates the server positions
-        p1slocation = c1packetstack[0]
-        if(c1packetstack[1]):
-            # If valid returns true and calls the shot for the server, and registers a kill
-            if((p1slocation + 3) <= (p2slocation + 5) and (p1slocation + 3) >= (p2slocation)):
-                c1kills  = c1kills + 1
-                c2deaths = c2deaths + 1
-            else:
-                c1killmiss = c1killmiss + 1
+        if(c1packetstack):
+            p1slocation = c1packetstack[0] + winsplitdist
+            if(c1packetstack[1]):
+                # If valid returns true and calls the shot for the server, and registers a kill
+                if((p1slocation + 3) <= (p2slocation + 5) and (p1slocation + 3) >= (p2slocation)):
+                    p1_shoot_draw(stdscr, 2)
+                    c1kills  = c1kills + 1
+                    c2deaths = c2deaths + 1
+                else:
+                    c1killmiss = c1killmiss + 1
 
     num = randint(0, 100)
-    if(num < c2packetloss):
+    if(num < c2packet):
+        c2packetloss = c2packetloss + 1
         c2packetstack = None
     else:
-        # Updates the server positions
-        p2slocation = c2packetstack[0]
-        if(c1packetstack[1]):
-            # If valid returns true and calls the shot for the server, and registers a kill
-            if((p2slocation + 3) <= (p1slocation + 5) and (p2slocation + 3) >= (p1slocation)):
-                c2kills  = c2kills + 1
-                c1deaths = c1deaths + 1
-            else:
-                c2killmiss = c2killmiss + 1
+        if(c2packetstack):
+            # Updates the server positions
+            p2slocation = c2packetstack[0] - winsplitdist
+            if(c2packetstack[1]):
+                # If valid returns true and calls the shot for the server, and registers a kill
+                if((p2slocation + 3) <= (p1slocation + 5) and (p2slocation + 3) >= (p1slocation)):
+                    p2_shoot_draw(stdscr, 2)
+                    c2kills  = c2kills + 1
+                    c1deaths = c1deaths + 1
+                else:
+                    c2killmiss = c2killmiss + 1
+
+    # For simulation purposes I'm going to have the server -> client packets always send 100% of the time. This is
+    # not realistic, but this simulation has limitations
+    p1c2location = p1slocation + winsplitdist
+    p2c1location = p2slocation - winsplitdist
+
+    # Updates the next server tick based on the tick rate
+    nextservertick = nextservertick + tickrate
 
 # ====================================== Running and Drawing ==========================================
 # Initial screen draw on load
@@ -345,12 +374,22 @@ def draw_screen(stdscr):
 # Redraws things that need to be redrawn
 def redraw_screen(stdscr):
     stdscr.clear()
+
     # Redraws guide lines
     redraw_lines(stdscr)
+
     # Redraws the labels
     server_init(stdscr)
+
     # Draws the server tick
     draw_tick(stdscr)
+
+    # Prints debug info in debug mode
+    if(debug):
+        debug_info(stdscr)
+
+    # Refreshes the draw window
+    stdscr.refresh()
 
 # This function is the actual running code. What this does is it has an arbitrary number that counts
 # up called the "loop tick", because it's very difficult for me to simulate actual server ticks.
@@ -359,11 +398,13 @@ def run_game(stdscr):
     global c1kills, c2kills
     global looptick, servertick
     global maxkills
+    global nextc1packet, nextc2packet, nextservertick
 
     while(c1kills <= maxkills and c2kills <= maxkills):
         # Move each player
         player_move(stdscr)
 
+        # Updates the position with the server
         if(nextc1packet == looptick):
             c1_send_packet(stdscr)
             c1_update_ping()
@@ -377,7 +418,13 @@ def run_game(stdscr):
             server_process(stdscr)
 
         redraw_screen(stdscr)
-        time.sleep(0.3)
+
+        # Delete the following at your own peril. Screen will struggle to update.
+        # This stalls in seconds. Use for debugging.
+        # If you want to step through each frame uncomment the next line
+        # stdscr.getch()
+        time.sleep(0.1)
+
         looptick = looptick + 1
 
 # Every server tick, the entire screen needs to refresh.
@@ -418,13 +465,11 @@ def draw_tick(stdscr):
     # Each client decides whether or not to shoot based on if there is a target to hit in front of them
     if(p1_shoot_logic()):
         p1_shoot_draw(stdscr, 1)
+
     if(p2_shoot_logic()):
         p2_shoot_draw(stdscr, 3)
 
     update_stats(stdscr)
-
-    # Refreshes the draw window
-    stdscr.refresh()
 
 # Every tick, needs to update the bottom row stats
 def update_stats(stdscr):
@@ -447,10 +492,16 @@ def update_stats(stdscr):
     stdscr.addstr((winheight - 6), (winsplitdist + (winsplitdist / 2) - 5), str(c2packetloss))
     stdscr.addstr((winheight - 6), ((winsplitdist * 2) + (winsplitdist / 2) - 5), str(c2killmiss))
 
+def debug_info(stdscr):
     stdscr.addstr(1, 1, str(looptick))
-
-    # Refreshes the draw window
-    stdscr.refresh()
+    stdscr.addstr(2, 1, "Player 1 Locations c1/s/c2")
+    stdscr.addstr(3, 1, str(p1c1location))
+    stdscr.addstr(4, 1, str(p1slocation))
+    stdscr.addstr(5, 1, str(p1c2location))
+    stdscr.addstr(6, 1, "Player 2 Locations c1/s/c2")
+    stdscr.addstr(7, 1, str(p2c1location))
+    stdscr.addstr(8, 1, str(p2slocation))
+    stdscr.addstr(9, 1, str(p2c2location))
 
 # ====================================== Starts This Bad Boy ==========================================
 # Calls the function, starts the program
